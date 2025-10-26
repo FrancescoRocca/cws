44a456041c0bbd18c1d2395be00e58553bc30712
Francesco <inbox@francescorocca.me>
2025-10-26 18:44:00 +0100

    feat(mime): add favicon mimetype

--- src/http/mime.c
+++ src/http/mime.c
@@ -5,11 +5,9 @@
 
 #include "http/request.h"
 
-static mimetype mimetypes[] = {{"html", "text/html"},
-							   {"css", "text/css"},
-							   {"js", "application/javascript"},
-							   {"jpg", "image/jpeg"},
-							   {"png", "image/png"}};
+static mimetype mimetypes[] = {
+	{"html", "text/html"}, {"css", "text/css"},	 {"js", "application/javascript"},
+	{"jpg", "image/jpeg"}, {"png", "image/png"}, {"ico", "image/x-icon"}};
 
 int http_get_content_type(char *location_path, char *content_type) {
 	/* Find last occurrence of a string */
@@ -24,6 +22,8 @@ int http_get_content_type(char *location_path, char *content_type) {
 			snprintf(content_type, CWS_HTTP_CONTENT_TYPE - 1, "%s", mimetypes[i].type);
 		}
 	}
+
+	snprintf(content_type, CWS_HTTP_CONTENT_TYPE - 1, "%s", "Content-Type not supported");
 
 	return 0;
 }
